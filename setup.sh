3c1726b5e9095010d61ec11c503861cfc1dcbcfa
Top-Slayer <dragontops55@gmail.com>
2025-12-05 15:54:11 +0700

    add: style for lua

--- setup.sh
+++ setup.sh
@@ -1,6 +1,11 @@
 command -v sudo >/dev/null 2>&1 || { echo "Not found 'sudo' command"; exit; }
 
 if command -v nix >/dev/null 2>&1; then
+	echo "nix exists"
+else
+	curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+
+if command -v nix >/dev/null 2>&1; then
     echo "nix exists"
 else
     sh <(curl --proto '=https' --tlsv1.2 -L https://nixos.org/nix/install) --no-daemon
